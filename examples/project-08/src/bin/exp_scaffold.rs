33ca16f92985eeedf41e949c89c6eb228c11e196
Guo Zhang <zhangguo@quanttide.com>
2026-06-08 19:13:35 +0800

    sessions are part of data/formal/

--- examples/project-08/src/bin/exp_scaffold.rs
+++ examples/project-08/src/bin/exp_scaffold.rs
@@ -305,7 +305,7 @@ fn save_turn(session_path: &str, turn: &Turn) {
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let data_path = "data/formal/scaffold-data.json";
-    let sessions_dir = "data/sessions";
+    let sessions_dir = "data/formal";
 
     let data: ScaffoldData =
         serde_json::from_str(&fs::read_to_string(data_path)?)?;
